1ba45da9e38b6b9697e46657e2646efc2419929b
BroccoliGuy <votre.email@example.com>
2024-10-28 14:48:59 +0100

    resultat nav

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,33 +1,33 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import '../styles/Results.css';
 
 interface Question {
   img: string;
   correctAnswer: string;
   options: string[];
-  userAnswers?: string; // Assurez-vous que cette propriété est optionnelle
+  userAnswers?: string;
 }
 
 const Results: React.FC = () => {
   const location = useLocation();
-  const { score = 0, questions = [] } = location.state || {}; // Valeurs par défaut
+  const navigate = useNavigate();
+  const { score = 0, questions = [] } = location.state || {};
 
   return (
     <div className="results-container">
       <div className="score">Score: {score} / {questions.length}</div>
-      <button className="restart-button" onClick={() => window.location.href = '/Plomesque/home'}>Recommencer</button>
+      <button className="restart-button" onClick={() => navigate('/home')}>Recommencer</button>
       <h2>Résultats du Quiz</h2>
       <div className="answers-container">
         {questions && questions.map((question: Question, index: number) => (
           <div key={index} className="question-container">
-            {/* Nouveau header avec numéro de question et nom du thème */}
             <img src={question.img} alt={`Question ${index + 1}`} />
             <div className="options-grid">
               {question.options.map((option, idx) => {
                 const isCorrect = option === question.correctAnswer;
-                const isSelected = question.userAnswers && question.userAnswers.includes(option); // Vérifiez que userAnswers n'est pas undefined
-                const showCorrectInOrange = !isSelected && isCorrect; // Affiche en orange si non sélectionné et est la bonne réponse
+                const isSelected = question.userAnswers && question.userAnswers.includes(option);
+                const showCorrectInOrange = !isSelected && isCorrect;
 
                 return (
                   <div
